d44155c92fb4dbc9fe47fe655cacf6a4a6ea4bc9
evan <malcomchege0582@gmail.com>
2024-09-03 09:49:41 +0300

    meet-ups

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+export default function Home() {
+  return (
+    <main className="p-3">
+      <div className="mt-5">
+      <Button>Click me</Button>
+    </div>
+    </main>
+  );
+}
